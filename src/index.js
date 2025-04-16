920a246cb8bf7e83791c1329d11df78916d5c95a
piyush gupta <piyushgupta8101@gmail.com>
2025-04-16 17:43:49 +0530

    Remove node_modules from Git tracking

---
+++ src/index.js
@@ -0,0 +1,87 @@
+import connectDB from "./db/index.js";
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+//now import express also
+import mongoose from 'mongoose'
+import { DB_NAME } from './constants'   
+
+import express from 'express'    //basic express js
+const app = express()
+
+ ( async () => {              //first use IIFE function with async then aplly try and catch
+    try{
+      await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)   //DB connecting in try    
+    
+    app.on("error", (error) =>{             //if not connecting with the app then throw error
+        console.log("ERROR:" ,error)
+        throw error
+    })   
+    app.listen(process.env.PORT, () =>{                   //basic express js
+        console.log(`App is listening on Port ${process.env.PORT}`);
+        
+    })  
+    }
+    catch(error){
+        console.error("ERROR:" ,error)            //throw error if any error occured with mongoose connect
+        throw error
+    }
+ })()
+    */
+
+
+
+
+
+
+/*
+
+//this is first method to connect database
+import mongoose from 'mongoose'
+import { DB_NAME } from './constants'   
+
+ ( async () => {              //first use IIFE function with async then aplly try and catch
+    try{
+      await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)   //DB connecting in try    
+    }
+    catch(error){
+        console.error("ERROR:" ,error)            //throw error if any error occured with mongoose connect
+        throw error
+    }
+ })()
+
+ */
